f03b80977d1124abfbaa2e6adc83ada31fae6716
Cade Weinberg <cadeweinberg@gmail.com>
2025-04-03 15:39:27 -0700

    stubbing out the api of stream

--- include/support/types/stream.h
+++ include/support/types/stream.h
@@ -14,3 +14,25 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with exp-support.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * @file support/types/stream.h
+ * @brief input/output streams.
+ */
+
+#ifndef SUPPORT_TYPES_STREAM_H
+#define SUPPORT_TYPES_STREAM_H
+
+#include "support/types/result.h"
+#include "support/types/string_view.h"
+
+struct Stream_;
+typedef struct Stream_ *Stream;
+
+Result stream_open(Stream *stream, StringView path, StringView mode);
+Result stream_close(Stream stream);
+Result stream_length(Stream stream, u64 *length);
+Result stream_write(Stream stream, StringView view);
+Result stream_read(Stream stream, void *buffer, u64 size, u64 *bytes_read);
+
+#endif // !SUPPORT_TYPES_STREAM_H
